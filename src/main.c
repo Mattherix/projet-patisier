ce4eb7474196efd4cf1ca3523f943791fe57239d
mattherix <mattherix@protonmail.com>
2021-10-06 16:33:40 +0200

    Add main.c + main programme structures

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Remplir goûts
+
+// Ajout commande à file cmd
+
+// While file not empty
+//      Récupe cmd
+//      Création gateau
+//      Livraison
+//      Dégustation

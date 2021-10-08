ed9e8e46dc6a1d859de4c491f35b8950d7676098
mattherix <mattherix@protonmail.com>
2021-10-08 23:05:11 +0200

    Rename file.c file.h

--- src/file.h
+++
@@ -1,8 +0,0 @@
-#include "element-str.h"
-
-typedef struct File_Commandes{
-    Element_str* commandes;
-} File_Commandes;
-
-File_Commandes* creer_file_commande();
-int est_vide_commande(File_Commandes* p);
